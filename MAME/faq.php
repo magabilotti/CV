a4971d983310b80c88da43c553ef162589d088f0
magabilotti <fmagalibilotti@gmail.com>
2022-08-10 23:03:24 -0300

    seccion de FAQ-MAME

---
+++ MAME/faq.php
@@ -0,0 +1,245 @@
+<?php
+?>
+
+<!-- instalador Bootstrap -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+<!-- css -->
+<style>
+
+.accordion-button:not(.collapsed) {
+  color: #0073a5;
+  background-color: #ddd;
+}
+
+.element.style {
+  font-weight:100;
+}
+</style>
+
+<!-- titulo -->
+
+<div class="" style="padding: 60px 40px; margin-top: -15px;"></div>
+<br>
+<br>
+<div><h3 class=" text:bold text-center text-color:black --bs-text-opacity:0,5">PREGUNTAS FRECUENTES</h3></div>
+
+<!-- Acordion -->
+<div class="" style="padding: 60px 200px; margin-top: -15px;">
+<div class="accordion" id="accordionExample">
+  <!-- pregunta 1 -->
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingUno">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseUno" aria-expanded="true" aria-controls="collapseUno">
+        <h5>Quiero trabajar abordo de un buque como tripulante &iquest;D&oacute;nde debo estudiar?</h5>
+      </button>
+    </h2>
+    <div id="collapseUno" class="accordion-collapse collapse show" aria-labelledby="headingUno" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <p>Entre otros establecimientos educativos existentes en nuestro pa&iacute;s, la Prefectura Naval Argentina cuenta con 28 Escuelas de Formaci&oacute;n y Capacitaci&oacute;n para el Personal Embarcado de la Marina Mercante (EFOCAPEMM) distribuidas a lo largo del pa&iacute;s, donde, luego de cursar en forma presencial el ciclo lectivo de OCHO (08) meses de duraci&oacute;n, se egresa con el Certificado de Capacidad N&aacute;utica de MARINERO o AUXILIAR DE M&AacuteQUINAS, dependiendo de la Especializaci&oacute;n elegida por el interesado.</p>
+        <p>Esta formaci&oacute;n tiene correspondencia con los T&iacute;tulos exigidos a nivel internacional acorde lo normado en el Convenio Internacional sobre Normas de Formaci&oacute;n, Titulaci&oacute;n y Guardia para la Gente de Mar (Convenio STCW).</p>
+
+      </div>
+    </div>
+  </div>
+  <!-- pregunta 2 -->
+  <div class="accordion-item">
+    <h1 class="accordion-header" id="headingDos">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseDos" aria-expanded="false" aria-controls="collapseDos">
+        <h5>&iquest;Qu&eacute; asignaturas integran los programas para obtener el Certificado de Capacidad N&aacute;utica de Marinero o Auxiliar de M&aacute;quinas?</h5>
+      </button>
+    </h1>
+    <div id="collapseDos" class="accordion-collapse collapse" aria-labelledby="headingDos" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <p>El a&ntilde;o lectivo se divide en dos ciclos lectivos de CUATRO (04) meses cada uno.</p>
+        <p>El primero, inicia su cursada durante el mes de marzo a junio denominado &Prime;ciclo com&uacute;n&Prime;, est&aacute; integrado por todos los cursantes del periodo lectivo, y las asignaturas que lo integra son:</p>
+          <ul>
+            <li><p>Nomenclatura Marinera - Reglamentaci&oacute;n Mar&iacute;tima - Nociones de Pesca - Ingl&eacute;s - Primeros Auxilios B&aacute;sicos - T&eacute;cnicas de Supervivencia Personal - Formaci&oacute;n en la Prevenci&oacute;n y Lucha contra Incendios - Seguridad Personal y Responsabilidades Sociales y Protecci&oacute;n del Buque e Instalaciones Portuarias.</p></li>
+          </ul>
+        <p>El segundo ciclo, de agosto a noviembre denominado &Prime;Per&iacute;odo Especializaci&oacute;n MARINERO&Prime; y &Prime;Per&iacute;odo Especializaci&oacute;n AUXILIAR DE M&Aacute;QUINAS&Prime;, se cursa luego de la elecci&oacute;n que cada alumno necesariamente debe hacer, y las asignaturas que las integra son, para:</p>
+          <ul>
+            <li><p><strong >MARINERO:</strong> Pr&aacute;ctica de la Navegaci&oacute;n - Conocimientos generales sobre Maniobras - Luces de Navegaci&oacute;n, Se&ntilde;alizaci&oacute;n, Faros y Balizas - Cabuller&iacute;a y Pr&aacute;cticas con Jarcias de labor.</p></li>
+            <li><p><strong>AUXILIAR DE M&Aacute;QUINAS:</strong> Motores Marinos - Instrumental de Sala de M&aacute;quinas -Lubricaci&oacute;n y Refrigeraci&oacute;n - Guardia en Sala de M&aacute;quinas.</p></li>
+          </ul>
+      </div>
+    </div>
+  </div>
+  <!-- pregunta 3 -->
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTres">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTres" aria-expanded="false" aria-controls="collapseTres">
+        <h5>&iquest;D&oacute;nde debo inscribirme para ingresar a la EFOCAPEMM?</h5>
+      </button>
+    </h2>
+    <div id="collapseTres" class="accordion-collapse collapse" aria-labelledby="headingTres" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <p>Para llevar a cabo la inscripci&oacute;n debe acercarse de forma presencial a la Escuela de Formaci&oacute;n y Capacitaci&oacute;n para el Personal Embarcado de la Marina Mercante (EFOCAPEMM) m&aacute;s cercana a su domicilio.</p>
+        <p>Para mayor informaci&oacute;n puede encontrar las Escuelas existentes haciendo click <a href = "https://marina-mercante.iusm.edu.ar/index.php?estado=4">ACA</a>, donde se desplegar&aacute; los contactos pertinentes y podr&aacute; enviar un correo electr&oacute;nico a la respectiva Escuela para consultas con mayor detalle.</p>
+      </div>
+    </div>
+  </div>
+  <!-- pregunta 4 -->
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingCuatro">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseCuatro" aria-expanded="false" aria-controls="collapseCuatro">
+        <h5>&iquest;Cu&aacute;l es el costo de la matr&iacute;cula de inscripci&oacute;n, cuota mensual y derechos de examen?</h5>
+      </button>
+    </h2>
+    <div id="collapseCuatro" class="accordion-collapse collapse" aria-labelledby="headingCuatro" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <p>Cursar el a&ntilde;o lectivo en cualquier EFOCAPEMM del pa&iacute;s, tanto para MARINERO como para AUXILIAR DE M&Aacute;QUINAS, es totalmente GRATUITO, no requiere ning&uacute;n abono o pago.</p>
+        <p>Vale aclarar que la emisi&oacute;n del Certificado de Capacidad N&aacute;utica respectivo si posee un arancel, el cual ser&aacute; abonado siguiendo las pautas descriptas haciendo click <a href="https://www.argentina.gob.ar/prefecturanaval/preguntas-frecuentes">ACA</a> y se puede verificar su pago haciendo click <a href="https://erecauda.prefecturanaval.gob.ar/erecauda/bat_form.php">ACA </a></p>
+      </div>
+    </div>
+  </div>
+  <!-- pregunta 5 -->
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingCinco">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseCinco" aria-expanded="false" aria-controls="collapseCinco">
+        <h5>&iquest;Qu&eacute; labores puedo desarrollar como tripulante de un buque con los Certificados de Capacidad N&aacute;utica obtenidos luego de cursar en las EFOCAPEMM?</h5>
+      </button>
+    </h2>
+    <div id="collapseCinco" class="accordion-collapse collapse" aria-labelledby="headingCinco" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <p>El Decreto N&deg; 572/94 &Prime;Reglamento de Formaci&oacute;n y Capacitaci&oacute;n del Personal Embarcado de la Marina Mercante&Prime; (REFOCAPEMM), el cual puede bajarse haciendo click <a href="https://www.argentina.gob.ar/normativa/nacional/decreto-572-1994-11658/texto">ACA</a> establece los cargos que se pueden desempe&ntilde;ar como tripulante de un buque afectado a la navegaci&oacute;n de cabotaje en funci&oacute;n del Certificado de Capacidad N&aacute;utica que se posea, son los denominados M&Aacute;XIMOS DE CARGO.</p>
+        <p>En buques de navegaci&oacute;n internacional podr&aacute; ocupar funciones como marinero de puente o marinero de m&aacute;quinas, seg&uacute;n la formaci&oacute;n que lleve a cabo.</p>
+      </div>
+    </div>
+  </div>
+  <!-- pregunta 6 -->
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingSeis">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeis" aria-expanded="false" aria-controls="collapseSeis">
+        <h5>&iquest;Cu&aacute;les son los requisitos para ingresar a la EFOCAPEMM?</h5>
+      </button>
+    </h2>
+    <div id="collapseSeis" class="accordion-collapse collapse" aria-labelledby="headingSeis" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <ul>
+          <li><p>Edad m&iacute;nima de DIECIOCHO (18) a&ntilde;os.
+          <li><p>Tener estudios secundarios completos.
+          <li><p>No poseer antecedentes profesionales desfavorables comprobados.
+          <li><p>No registrar antecedentes policiales o judiciales cuya naturaleza indique como no aconsejable su inscripci&oacute;n.
+          <li><p>Saber nadar y remar.
+          <li><p>Aprobar el examen de aptitud psicof&iacute;¬sica acorde las condiciones exigidas por la Ordenanza Mar&iacute;tima 02/13 tomo 5.
+          <li><p>Aprobar el examen intelectual.
+          <li><p>El personal aspirante a empleos sin requisito de capacitaci&oacute;n, que se inscriba deber&aacute; presentar certificado que acredite idoneidad en la especialidad o certificaci&oacute;n de trabajo en tareas afines por un per&iacute;odo no menor a un a&ntilde;o.
+          <li><p>Para el caso de aspirantes extranjeros, deber&aacute;n presentar la documentaci&oacute;n legal correspondiente y la que exija el Convenio Internacional sobre Normas de Formaci&oacute;n, Titulaci&oacute;n y Guardia para la Gente de Mar.</p></li>
+      </div>
+    </div>
+  </div>
+  <!-- pregunta 7 -->
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingSiete">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSiete" aria-expanded="false" aria-controls="collapseSiete">
+        <h5>Una vez obtenido el Certificado de Capacidad N&aacute;utica de Marinero o Auxiliar de M&aacute;quinas, &iquest;c&oacute;mo contin&uacute;a la carrera en la marina mercante?</h5>
+      </button>
+    </h2>
+    <div id="collapseSiete" class="accordion-collapse collapse" aria-labelledby="headingSiete" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        El Decreto N&deg; 572/94 &Prime;Reglamento de Formaci&oacute;n y Capacitaci&oacute;n del Personal Embarcado de la Marina Mercante&Prime; (REFOCAPEMM), el cual puede bajarse haciendo click <a href="https://www.argentina.gob.ar/normativa/nacional/decreto-572-1994-11658/texto">ACA</a>, establece los requisitos de ascenso para obtener una habilitaci&oacute;n de mayor alcance (M&aacute;ximos de cargo) que se podr&aacute;n desarrollar abordo, los cuales se encuentran divididos en: Ultramar - Fluvial y Pesca.
+      </div>
+    </div>
+  </div>
+  <!-- pregunta 8 -->
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOcho">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOcho" aria-expanded="false" aria-controls="collapseOcho">
+        <h5>&iquest;Se pueden realizar los Cursos OMI STCW en las EFOCAPEMM?</h5>
+      </button>
+    </h2>
+    <div id="collapseOcho" class="accordion-collapse collapse" aria-labelledby="headingOcho" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        S&iacute; se pueden realizar los cursos OMI STCW en las EFOCAPEMM, para conocer m&aacute;s detalles al respecto haga click <a href="https://marina-mercante.iusm.edu.ar/index.php?estado=6">ACA</a> donde encontrar&aacute; la oferta educativa, fechas, Escuelas donde se dictar&aacute;n, y sus respectivos contactos (correo electr&oacute;nico y telef&oacute;nico).
+      </div>
+    </div>
+  </div>
+  <!-- pregunta 9 -->
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingNueve">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseNueve" aria-expanded="false" aria-controls="collapseNueve">
+        <h5>&iquest;Cu&aacute;l es el costo para realizar los cursos STCW en una EFOCAPEMM?</h5>
+      </button>
+    </h2>
+    <div id="collapseNueve" class="accordion-collapse collapse" aria-labelledby="headingNueve" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        Los cursos STCW que son desarrollados en las EFOCAPEMM son GRATUITOS. Vale aclarar que la emisi&oacute;n del Certificado correspondiente al curso que apruebe tiene un arancel, el cual ser&aacute; abonado siguiendo las pautas descriptas haciendo click <a href="https://www.argentina.gob.ar/prefecturanaval/preguntas-frecuentes">ACA</a> y se puede verificar su pago haciendo click <a href="https://erecauda.prefecturanaval.gob.ar/erecauda/bat_form.php">ACA</a>.
+
+      </div>
+    </div>
+  </div>
+  <!-- pregunta 10 -->
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingDiez">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseDiez" aria-expanded="false" aria-controls="collapseDiez">
+        <h5>&iquest;Los certificados de los cursos OMI STCW emitidos por una EFOCAPEMM son de alcance nacional o internacional?</h5>
+      </button>
+    </h2>
+    <div id="collapseDiez" class="accordion-collapse collapse" aria-labelledby="headingDiez" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        Los Certificados STCW emitidos por una EFOCAPEMM son de alcance nacional e internacional, vale decir, deben ser aceptados para embarcar como tripulante en buques que enarbolen pabell&oacute;n de cualquier bandera.
+      </div>
+    </div>
+  </div>
+  <!-- pregunta 11 -->
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOnce">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOnce" aria-expanded="false" aria-controls="collapseOnce">
+        <h5>&iquest;Cu&aacute;l es la modalidad de cursada?</h5>
+      </button>
+    </h2>
+    <div id="collapseOnce" class="accordion-collapse collapse" aria-labelledby="headingOnce" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        La oferta educativa desarrollada en las EFOCAPEMM responde a programas debidamente aprobados por la Autoridad de Aplicaci&oacute;n del REFOCAPEMM (Subsecretar&iacute;a de Puertos, V&iacute;as Navegables y Marina Mercante - Ministerio de Transporte), en la modalidad &Prime;PRESENCIAL&Prime;; tal es as&iacute; que, a la fecha no existen programas aprobados para ser dictados en forma &Prime;VIRTUAL&Prime; o &Prime;A DISTANCIA&Prime;.
+      </div>
+    </div>
+  </div>
+  <!-- pregunta 12 -->
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingDoce">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseDoce" aria-expanded="false" aria-controls="collapseDoce">
+        <h5>&iquest;D&oacute;nde y/o como convalido mis certificados de los cursos OMI STCW emitidos por una EFOCAPEMM?</h5>
+      </button>
+    </h2>
+    <div id="collapseDoce" class="accordion-collapse collapse" aria-labelledby="headingDoce" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <p>Los Certificados STCW expedidos pueden ser convalidados iniciando el tr&aacute;mite - en forma personal - a trav&eacute;s de cualquier EFOCAPEMM (aquella donde hizo los cursos u otra escuela distinta), siempre de forma presencial. Otra forma de realizar esta convalidaci&oacute;n es acercarse hasta la Direcci&oacute;n de Educaci&oacute;n de la Prefectura Naval Argentina, sita en Avenida Corrientes 345 - Piso 12 - Ciudad Aut&oacute;noma de Buenos Aires.</p>
+        <p>Existe tambi&eacute;n la posibilidad de diligenciar su solicitud ante la Autoridad de Administraci&oacute;n y Ejecuci&oacute;n del REFOCAPEMM (Armada Argentina) quien canalizar&aacute; el tr&aacute;mite por el sistema de Gesti&oacute;n Documental Electr&oacute;nica (GDE) a la Prefectura Naval Argentina (Direcci&oacute;n de Educaci&oacute;n).</p>
+      </div>
+    </div>
+  </div>
+  <!-- pregunta 13 -->
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTrece">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTrece" aria-expanded="false" aria-controls="collapseTrece">
+        <h5>&iquest;Cu&aacute;l es el costo del tr&aacute;mite para convalidar los certificados de los cursos OMI STCW expedido por una EFOCAPEMM?</h5>
+      </button>
+    </h2>
+    <div id="collapseTrece" class="accordion-collapse collapse" aria-labelledby="headingTrece" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        El tr&aacute;mite para convalidar los Certificados STCW expedidos por una EFOCAPEMM es totalmente GRATUITO.
+      </div>
+    </div>
+  </div>
+  <!-- pregunta 14 -->
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingCatorce">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseCatorce" aria-expanded="false" aria-controls="collapseCatorce">
+        <h5>&iquest;Porqu&eacute; necesito convalidar los Certificados de los Cursos STCW realizados en una EFOCAPEMM?</h5>
+      </button>
+    </h2>
+    <div id="collapseCatorce" class="accordion-collapse collapse" aria-labelledby="headingCatorce" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        El requerimiento de convalidar los Certificados de los Cursos STCW realizados en una EFOCAPEMM, es exigido por la Armada Argentina en Expedientes tramitados ante la Subsecretar&iacute;a de Formaci&oacute;n, Capacitaci&oacute;n y Titulaci&oacute;n del Personal Embarcado de la Marina Mercante.
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+
+
+<?php
+include_once "web/int/pie.php";
+?>
